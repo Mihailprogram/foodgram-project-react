e3441ed0b9a36c7fb0bd4dfd91631d53796ad384
Михаил Химач <miha.vjk@mail.ru>
2023-06-27 13:31:03 +0500

    up

---
+++ backend/users/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+admin.site.unregister(User)
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        'id', 'username', 'email',
+        'first_name', 'last_name', 'date_joined',)
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    list_filter = ('date_joined', 'email', 'first_name')
+    empty_value_display = '-пусто-'
---
+++ backend/users/models.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
+class СustomUser(AbstractUser):
+    email = models.EmailField(
+        'Email',
+        max_length=200,
+        unique=True,
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name='customuser_set'  
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name='customuser_set'  
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
